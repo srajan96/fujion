a69f849c75838e5259965b1a99298ee3bda5c316
srajan96 <srajan1996@gmail.com>
2017-02-23 23:59:44 +0530

    Register.java servlet added....may not work since no suitable driver found message came back again. Servlet now packed inside the folder.

---
+++ src/java/register.java
@@ -0,0 +1,111 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Srajan
+ */
+public class register extends HttpServlet {
+    //Database connection parameters
+    final String dbPassword="leopard";
+    final String dbName="fusion";
+    final String dbUser="root";
+    final String dbPath="localhost:3306";
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+ 
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String name=request.getParameter("name");
+        String username=request.getParameter("username");
+        String email=request.getParameter("email");
+        String password=request.getParameter("password");
+        
+        try {
+            /* TODO output your page here. You may use following sample code. */
+             Class.forName("com.mysql.jdbc.Driver");
+             Connection conn=DriverManager.getConnection("jdbc:mysql//"+dbPath+"/"+dbName,dbUser,dbPassword);
+             PreparedStatement updateemp =conn.prepareStatement("insert into users(username,password,name,email)values(?,?,?,?)"); 
+             updateemp.setString(1,username);
+             updateemp.setString(2,password);
+             updateemp.setString(3, name);
+             updateemp.setString(4, email);
+             updateemp.executeUpdate();
+             String op="{registered:true}";
+             out.println(op);
+        } 
+        catch(Exception e){
+            out.println(e);
+           e.printStackTrace();
+        }
+        finally {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(register.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(register.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
